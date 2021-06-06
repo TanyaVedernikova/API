55d569467251bda412bd344121638315e328d995
TanyaVedernikova <vedernikovatatyananik@gmail.com>
2021-06-06 14:59:35 +0500

    new fixs add json data file

--- api/main.py
+++ api/main.py
@@ -1,69 +1,29 @@
 import cherrypy
+import json
 
-list_of_users = [
-    {'id': 0,
-    'username': 'Bloom',
-    'email': 'crazyfrog@dot.com',
-    'department': 'Fire fairy',
-    'date_joined': '2020-09-10'
-    },
-    {'id': 1,
-    'username': 'Stella',
-    'email': 'deadinside228@mail.ru',
-    'department': 'Light fairy',
-    'date_joined': '2020-09-11'
-    },
-    {'id': 2,
-    'username': 'Musa',
-    'email': 'dillerganjubas@gmail.com',
-    'department': 'Music fairy',
-    'date_joined': '2020-09-12'
-    },
-    {'id': 3,
-     'username': 'Flora',
-     'email': 'pes_blatnoy@gmail.com',
-     'department': 'Nature fairy',
-     'date_joined': '2020-09-13'
-     },
-    {'id': 4,
-     'username': 'Techna',
-     'email': 'polupoker_and_chilipizdric@gmail.com',
-     'department': 'Technology fairy',
-     'date_joined': '2020-09-14'
-     },
-]
+
+
 
 @cherrypy.tools.json_out()
 @cherrypy.expose
 class Users:
 
+    path = 'data_users.json'
+    with open(path, 'r') as f:
+        data_users = json.loads(f.read())
+
+
     def GET(self, username = None, department = None):
-        if username == None and department == None:
-            list_of_username = []
-            for items in list_of_users:
-                list_of_username.append(items.get('username'))
-            return list_of_username
-
-        elif department != None and username != None:
-            list_of_username = []
-            for items in list_of_users:
-                if items.get('username').find(username) != -1 and items.get('department').find(department) != -1:
-                    list_of_username.append(items)
-            return list_of_username
-
-        elif username != None:
-            list_of_username = []
-            for items in list_of_users:
-                if items.get('username').find(username) != -1:
-                    list_of_username.append(items)
-            return list_of_username
-
-        elif department != None:
-            list_of_username = []
-            for items in list_of_users:
-                if items.get('department').find(department) != -1:
-                    list_of_username.append(items)
-            return list_of_username
+        if username == None:
+            data1 = Users.data_users["users"]
+        else:
+            data1 = [user for user in Users.data_users["users"] if user["username"].find(username) != -1]
+        if department == None:
+            data2 = Users.data_users["users"]
+        else:
+            data2 = [user for user in Users.data_users["users"] if user["department"].find(department) != -1]
+        data3 = [user for user in data1 if user in data2]
+        return data3
 
 @cherrypy.tools.json_out()
 @cherrypy.expose
@@ -83,21 +43,6 @@ class Department:
 
 
 
-
-cherrypy.tree.mount(
-    Users(), '/users', {
-        '/':
-            {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
-    }
-),
-cherrypy.tree.mount(
-    Department(), '/department', {
-        '/':
-            {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
-    }
-)
-cherrypy.config.update({'server.socket_host':'0.0.0.0', 'server.socket_port': 8080})
-
 def start_api():
     cherrypy.engine.start()
     cherrypy.engine.block()
@@ -105,4 +50,23 @@ def finish_api():
     cherrypy.engine.stop()
 
 
+
+
+
+cherrypy.tree.mount(
+    Users(), '/users', {
+        '/':
+             {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
+       }
+),
+cherrypy.tree.mount(
+     Department(), '/department', {
+        '/':
+            {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
+    }
+
+)
+
 start_api()
+
+
